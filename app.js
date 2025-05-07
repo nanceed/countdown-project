cb648b8a5ae4f91d35d0ece320c5ccfafac3405c
Nancy <duong.nancy@gmail.com>
2025-05-07 12:37:18 -0700

    Updated JS functionality

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 //console.log(items);
 
-let futureDate = new Date(2025,4,24,15,30,0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+//let futureDate = new Date(2025,4,7,12,30,0);
 //console.log(futureDate);
 
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -27,7 +33,7 @@ const date = futureDate.getDate();
 //console.log(weekday);
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on | ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
+giveaway.textContent = `giveaway ends on | ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 
 // future time in ms
 const futureTime = futureDate.getTime();
@@ -37,7 +43,7 @@ function getRemainingTime() {
     const today = new Date().getTime();
     //console.log(today);
     const t = futureTime - today;
-    console.log(t);
+    //console.log(t);
     // 1s = 1000ms
     // 1m = 60s
     // 1hr = 60min
@@ -61,9 +67,23 @@ function getRemainingTime() {
     // set values array
     const values = [days, hours, minutes, seconds];
 
+    function format(item) {
+        if (item < 10) {
+            return item = `0${item}`;
+        }
+        return item
+    }
+
     items.forEach(function (item, index) {
-        item.innerHTML = values[index];
+        item.innerHTML = format(values[index]);
     });
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+    }
 }
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
 
 getRemainingTime();
